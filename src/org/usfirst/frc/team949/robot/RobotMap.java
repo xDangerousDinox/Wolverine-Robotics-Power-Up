a383587efb1aee2ee9ed87482002127c208628b6
David Cheng <davidcheng789@gmail.com>
2018-02-14 10:04:01 -0800

    mmmm

--- src/org/usfirst/frc/team949/robot/RobotMap.java
+++ src/org/usfirst/frc/team949/robot/RobotMap.java
@@ -20,10 +20,10 @@ public class RobotMap {
 	public final static int leftPickupMotor = 6;
 	public final static int rightPickupMotor = 7;
 	public final static int wristMotor = 9;
-	
+
 	// Arm
 	public final static int armMotor = 8;
-	
+
 	// Climber
 	public final static int climberMotor1 = 4;
 	public final static int climberMotor2 = 5;
